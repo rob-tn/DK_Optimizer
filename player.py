4f84334593a5e09552e42b216031a1dd47e66fcd
Robert Nguyen <rob0726@berkeley.edu>
2017-12-30 13:30:35 -0800

    initial commit

---
+++ player.py
@@ -0,0 +1,29 @@
+class player():
+
+    def __init__(self):
+        self.points = None
+        self.turnovers = None
+        self.assists = None
+        self.rebounds = None
+        self.blocks = None
+        self.steals = None
+
+    def getPoints(self):
+        return self.points
+
+    def getTurnovers(self):
+        return self.turnovers
+
+    def getAssists(self):
+        return self.assists
+
+    def getRebounds(self):
+        return self.rebounds
+
+    def getBlocks(self):
+        return self.blocks
+
+    def getSteals(self):
+        return self.steals
+
+    
